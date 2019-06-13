1914d8573c9180c31c442ce215bbc36038f436d0
Guewen Baconnier <guewen.baconnier@camptocamp.com>
2019-06-13 15:49:12 +0200

    Running pre-commit hooks on existing sources

--- odoo/local-src/cosanum_hr/models/hr_leave.py
+++ odoo/local-src/cosanum_hr/models/hr_leave.py
@@ -1,6 +1,6 @@
 # Copyright 2019 Camptocamp SA
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
-from odoo import models, api, _
+from odoo import _, api, models
 
 
 class HrLeave(models.Model):
@@ -9,9 +9,5 @@ class HrLeave(models.Model):
     @api.multi
     def _prepare_holidays_meeting_values(self):
         values = super()._prepare_holidays_meeting_values()
-        values.update(
-            {'name': _('On leave'),
-             'description': '',
-             }
-        )
+        values.update({'name': _('On leave'), 'description': ''})
         return values
