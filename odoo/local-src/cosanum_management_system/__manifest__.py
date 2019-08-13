14e3838579e408918d2ed9989f720a02c38248a6
jcoux <julien.coux@camptocamp.com>
2019-08-13 10:09:17 +0200

    BSCOS-285: Move some management system menus and rename knowledge menu

---
+++ odoo/local-src/cosanum_management_system/__manifest__.py
@@ -0,0 +1,21 @@
+# Copyright 2019 Camptocamp SA
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html).
+{
+    'name': 'Cosanum Management System',
+    'summary': 'Specific Management System for Cosanum',
+    'version': '12.0.1.0.0',
+    'category': 'Management System',
+    'author': 'Camptocamp',
+    'license': 'AGPL-3',
+    'depends': [
+        'document_page_procedure',
+        'document_page_work_instruction',
+        'mgmtsystem',
+        'mgmtsystem_manual',
+    ],
+    'website': 'http://www.camptocamp.com',
+    'data': [
+        'views/menus.xml'
+    ],
+    'installable': True,
+}
