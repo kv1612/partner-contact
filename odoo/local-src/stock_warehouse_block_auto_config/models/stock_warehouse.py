a1a154971c52ac238e914bbc649e186b811f92be
Guewen Baconnier <guewen.baconnier@camptocamp.com>
2019-12-20 14:18:57 +0100

    Merge --squash 'logistics' branch into 13.0
    All commits from the 12.0 logistics branch are squashed.
    We have too many conflicts using cherry-pick or rebase, and
    merge just adds more complexity when most of the commits in
    the logistics branch were only about updating data files,
    back and forth on songs and update on submodule. We have some
    code too, but part of it will be removed anyway. If we want
    the details of the commits, we can look in the 'logistics' branch.
    
    It will reduce the size of the diff as well, as large files were
    modified often.

---
+++ odoo/local-src/stock_warehouse_block_auto_config/models/stock_warehouse.py
@@ -0,0 +1,38 @@
+# Copyright 2019 Camptocamp SA
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html).
+
+from odoo import _, exceptions, fields, models
+
+
+class StockWarehouse(models.Model):
+    _inherit = "stock.warehouse"
+
+    block_delivery_route_update = fields.Boolean(
+        string="Block Incoming Shipments Configuration"
+    )
+    block_reception_route_update = fields.Boolean(
+        string="Block Outgoing Shipments Configuration"
+    )
+
+    def write(self, values):
+        for wh in self:
+            if (
+                values.get('block_delivery_route_update')
+                or wh.block_delivery_route_update
+            ) and 'delivery_steps' in values:
+                raise exceptions.UserError(_('Delivery Steps are blocked.'))
+
+            if (
+                values.get('block_reception_route_update')
+                or wh.block_reception_route_update
+            ) and 'reception_steps' in values:
+                raise exceptions.UserError(_('Reception Steps are blocked.'))
+        return super().write(values)
+
+    def _get_routes_values(self):
+        values = super()._get_routes_values()
+        if self.block_delivery_route_update:
+            values.pop('delivery_route_id', None)
+        if self.block_reception_route_update:
+            values.pop('reception_route_id', None)
+        return values
