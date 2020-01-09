95e3ec9ab5cc7eca587edad7a46f4a3ea3c8d0a5
Guewen Baconnier <guewen.baconnier@camptocamp.com>
2020-01-09 10:03:30 +0100

    Migrate cosanum_hr
    BSCOS-445

--- odoo/local-src/cosanum_hr/__manifest__.py
+++ odoo/local-src/cosanum_hr/__manifest__.py
@@ -3,7 +3,7 @@
 {
     'name': 'Cosanum HR',
     'summary': 'Specific HR for Cosanum',
-    'version': '12.0.1.0.0',
+    'version': '13.0.1.0.0',
     'category': 'HR',
     'author': 'Camptocamp',
     'license': 'AGPL-3',
@@ -14,12 +14,12 @@
         'hr_holidays',
         'hr_attendance_report_theoretical_time',
     ],
-    'website': 'http://www.camptocamp.com',
+    'website': 'https://www.camptocamp.com',
     'data': [
         'views/hr_attendance_views.xml',
         'views/hr_holidays_views.xml',
         'views/hr_attendance_theoretical_time_views.xml',
         'views/hr_holiday_report.xml'
     ],
-    'installable': False,
+    'installable': True,
 }
--- odoo/local-src/cosanum_hr/models/hr_leave.py
+++ odoo/local-src/cosanum_hr/models/hr_leave.py
@@ -1,12 +1,11 @@
 # Copyright 2019 Camptocamp SA
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
-from odoo import _, api, models
+from odoo import _, models
 
 
 class HrLeave(models.Model):
     _inherit = 'hr.leave'
 
-    @api.multi
     def _prepare_holidays_meeting_values(self):
         values = super()._prepare_holidays_meeting_values()
         values.update({'name': _('On leave'), 'description': ''})
