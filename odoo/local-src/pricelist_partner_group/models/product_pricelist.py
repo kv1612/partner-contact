39b3ed180dece206d46f91db14e35a8b338cfada
Matthieu Méquignon <matthieu.mequignon@camptocamp.com>
2020-06-24 15:48:11 +0200

    [BSCOS-1147] Update pricelist item prices from csv imports

--- odoo/local-src/pricelist_partner_group/models/product_pricelist.py
+++ odoo/local-src/pricelist_partner_group/models/product_pricelist.py
@@ -2,6 +2,10 @@
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl)
 
 from odoo import _, api, exceptions, models
+from odoo.tools import float_compare
+
+SINGLE_PRODUCT = "0_product_variant"
+ALL_PRODUCTS = "3_global"
 
 
 class ProductPricelist(models.Model):
@@ -10,10 +14,15 @@ class ProductPricelist(models.Model):
 
     def check_duplicate_item(self, vals):
         """
-        Search and returns existing product.pricelist.item
+        Search and returns existing product.pricelist.item records
         matching the vals passed as argument.
         """
-        domain = [(key, "=", val) for key, val in vals.items() if val]
+        domain = [
+            (key, "=", val)
+            for key, val in vals.items()
+            if val and key not in ["price_discount", "fixed_price"]
+        ]
+        domain.append(("pricelist_id", "=", self.id))
         return self.env["product.pricelist.item"].search(domain)
 
     def filter_existing_items(self, item_vals):
@@ -22,10 +31,33 @@ class ProductPricelist(models.Model):
         """
         item_ids = []
         for item in item_vals:
-            if self.check_duplicate_item(item[2]):
-                continue
-            item_ids.append(item)
+            # (0, False, vals) tuples
+            vals = item[2]
+            existing_item = self.check_duplicate_item(vals)
+            if existing_item:
+                self.update_prices(existing_item, vals)
+            else:
+                item_ids.append(item)
         return item_ids
+
+    def update_prices(self, item, vals):
+        """
+        Update pricelist item if prices was changed.
+        """
+        if vals.get("applied_on") == SINGLE_PRODUCT:
+            new_price = vals.get("fixed_price")
+            price_changed = new_price and float_compare(
+                new_price, item.fixed_price, precision_digits=2
+            )
+            if price_changed:
+                item.fixed_price = new_price
+        else:
+            new_discount = vals.get("price_discount")
+            discount_changed = new_discount and float_compare(
+                new_discount, item.price_discount, precision_digits=2
+            )
+            if discount_changed:
+                item.price_discount = new_discount
 
     def write(self, vals):
         if self.env.context.get("load_csv"):
@@ -43,8 +75,7 @@ class ProductPricelist(models.Model):
         for record in self:
             formulas = record.item_ids.filtered(
                 lambda i: i.compute_price == "formula"
-                # pricelist items applied on all products
-                and i.applied_on == "3_global"
+                and i.applied_on == ALL_PRODUCTS
             )
             if len(formulas) > 1:
                 raise exceptions.ValidationError(
