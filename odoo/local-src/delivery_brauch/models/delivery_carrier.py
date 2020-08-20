873ecfdffb9e82d7cf6e3a016955b5ba499d187f
Guewen Baconnier <guewen.baconnier@camptocamp.com>
2020-08-20 14:25:34 +0200

    Implement dummy delivery provider methods for brauch
    These methods are required even if they do not do anything (see specs in
    the delivery module).
    
    Avoid such errors:
    
      File "/odoo/src/addons/delivery/wizard/choose_delivery_carrier.py", line 44, in _onchange_order_id
        vals = self._get_shipment_rate()
      File "/odoo/src/addons/delivery/wizard/choose_delivery_carrier.py", line 69, in _get_shipment_rate
        if vals.get('success'):
    AttributeError: 'NoneType' object has no attribute 'get'
    
    For the rate, we always send 0, because anyway for cosanum the price is
    overriden by the module delivery_carrier_pricelist.

--- odoo/local-src/delivery_brauch/models/delivery_carrier.py
+++ odoo/local-src/delivery_brauch/models/delivery_carrier.py
@@ -24,6 +24,30 @@ class DeliveryCarrier(models.Model):
         "python expression with the 'object' and 'time' variables.",
     )
 
+    def brauch_rate_shipment(self, order):
+        carrier = self._match_address(order.partner_shipping_id)
+        if not carrier:
+            return {
+                "success": False,
+                "price": 0.0,
+                "error_message": _(
+                    "Error: this delivery method is not available for this address."
+                ),
+                "warning_message": False,
+            }
+        return {
+            "success": True,
+            "price": 0.0,
+            "error_message": False,
+            "warning_message": False,
+        }
+
+    def brauch_get_tracking_link(self, picking):
+        return False
+
+    def brauch_cancel_shipment(self, pickings):
+        raise NotImplementedError()
+
     def brauch_send_shipping(self, pickings):
         res = []
         for pick in pickings:
