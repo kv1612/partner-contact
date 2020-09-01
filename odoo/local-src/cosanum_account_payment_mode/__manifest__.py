e11756769d29d6897df153cce537e1b418ae6aa0
Thierry Ducrest <thierry.ducrest@camptocamp.com>
2020-09-01 12:16:00 +0200

    Add cosanum_account_payment_mode
    On an invoice when the payment mode is changed, it the mode is linked to
    a bank account, the bank account is automatically set on the invoice.
    If there is no bank account linked, nothing is done, so the default
    account will be used.
    
    BSCOS-1445

---
+++ odoo/local-src/cosanum_account_payment_mode/__manifest__.py
@@ -0,0 +1,17 @@
+# Copyright 2020 Camptocamp SA
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html)
+
+{
+    "name": "Cosanum Account Payment Mode",
+    "summary": "Cosanum customization for OCA payment mode modules.",
+    "version": "13.0.1.0.0",
+    "category": "Hidden",
+    "author": "Camptocamp",
+    "license": "AGPL-3",
+    "depends": [
+        "account_payment_mode",
+        "account_payment_partner",
+    ],
+    "website": "https://www.camptocamp.com",
+    "installable": True,
+}
---
+++ odoo/local-src/cosanum_account_payment_mode/tests/__init__.py
@@ -0,0 +1,1 @@
+from . import test_cosanum_account_payment_mode
