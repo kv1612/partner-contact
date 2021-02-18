b6dd63be0e459173b85ea741a40d85e2f8f9534b
Matthieu Méquignon <matthieu.mequignon@camptocamp.com>
2021-02-18 11:34:09 +0100

    Add sale_order_line_replacement module

---
+++ odoo/local-src/sale_order_line_replacement/__manifest__.py
@@ -0,0 +1,12 @@
+# Copyright 2021 Camptocamp SA
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl)
+
+{
+    "name": "Sale order line replacement",
+    "version": "14.0.1.0.0",
+    "category": "Sales Management",
+    "author": "Camptocamp",
+    "license": "AGPL-3",
+    "depends": ["sale"],
+    "installable": True,
+}
