ddbd4d2f32df6433355a0081946953ecb64eb53f
Simone Orsi <simahawk@gmail.com>
2021-03-04 11:04:26 +0100

    cosanum_delivery: prevent send docs to carrier in dev mode

--- odoo/local-src/cosanum_delivery/models/stock_picking.py
+++ odoo/local-src/cosanum_delivery/models/stock_picking.py
@@ -1,10 +1,29 @@
 # Copyright 2020 Camptocamp SA
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html).
 
-from odoo import fields, models
+import logging
+import os
+
+from odoo import fields, models, tools
+
+_logger = logging.getLogger(__name__)
 
 
 class StockPicking(models.Model):
     _inherit = "stock.picking"
 
     carrier_id = fields.Many2one(tracking=True)
+
+    def send_to_shipper(self):
+        # Overridden to prevent sending data to the shipper (to print labels
+        # for instance) in a dev environment.
+        # It is actually blocking the validation of DHL transfers but it could
+        # block others carrier integrations because they are all built the same
+        # way in odoo/enterprise (delivery_dhl, delivery_ups...).
+        if (
+            os.getenv("RUNNING_ENV") == "dev"
+            and not tools.config['test_enable']
+        ):
+            _logger.info("DEV MODE detected: skip send shipping")
+            return
+        return super().send_to_shipper()
