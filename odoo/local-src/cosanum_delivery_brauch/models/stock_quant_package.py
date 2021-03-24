a84f07e88b3231f61d851af7f0d58d1660cee8eb
Sébastien Alix <sebastien.alix@camptocamp.com>
2021-03-24 16:11:38 +0100

    Brauch, use of default delivery packaging if none
    If no delivery packaging is set on the package, use the default one
    configured on the carrier.
    
    In our case, "Brauch Palette" carrier is configured with the "Brauch
    Pallet" default delivery packaging.
    
    BSCOS-2204

--- odoo/local-src/cosanum_delivery_brauch/models/stock_quant_package.py
+++ odoo/local-src/cosanum_delivery_brauch/models/stock_quant_package.py
@@ -8,7 +8,10 @@ class StockQuantPackage(models.Model):
 
     _inherit = "stock.quant.package"
 
-    def _brauch_get_pack_data(self):
-        res = super()._brauch_get_pack_data()
-        res["Package Code"] = self.packaging_id.shipper_package_code or ""
+    def _brauch_get_pack_data(self, picking):
+        res = super()._brauch_get_pack_data(picking)
+        default_packaging = picking.carrier_id.brauch_default_packaging_id
+        res["Package Code"] = (
+            self.packaging_id or default_packaging
+        ).shipper_package_code or ""
         return res
