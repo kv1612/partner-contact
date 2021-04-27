2c8da79d7716e57549bbff5acc8d7730c76472d4
Jacques-Etienne Baudoux <je@bcim.be>
2021-04-27 19:18:05 +0200

    BSCOS-2494 Any shipping method must implement the <type>_rate_shipment method
    1/ Added for delivery_cosanum
    2/ In delivery_bruach, return the price defined on the shipping method instead of hardcoded 0

--- odoo/local-src/delivery_cosanum/models/delivery_carrier.py
+++ odoo/local-src/delivery_cosanum/models/delivery_carrier.py
@@ -1,7 +1,7 @@
 # Copyright 2021 Camptocamp SA
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl)
 
-from odoo import fields, models
+from odoo import _, fields, models
 
 
 class DeliveryCarrier(models.Model):
@@ -13,3 +13,25 @@ class DeliveryCarrier(models.Model):
         domain=[("package_carrier_type", "=", "cosanum")],
         string="Cosanum Default Packaging",
     )
+
+    def cosanum_rate_shipment(self, order):
+        self.ensure_one()
+        carrier = self._match_address(order.partner_shipping_id)
+        if not carrier:
+            return {
+                "success": False,
+                "price": 0.0,
+                "error_message": _(
+                    "Error: this delivery method is not available for this address."
+                ),
+                "warning_message": False,
+            }
+        delivery_product_price = (
+            self.product_id and self.product_id.lst_price or 0
+        )
+        return {
+            "success": True,
+            "price": delivery_product_price,
+            "error_message": False,
+            "warning_message": False,
+        }
