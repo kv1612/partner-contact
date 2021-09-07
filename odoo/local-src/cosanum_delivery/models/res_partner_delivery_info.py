eb8e86d8d396e4801024e132114a77eaa5610812
Simone Orsi <simahawk@gmail.com>
2021-09-07 16:01:58 +0200

    Add some TODOs for v14

--- odoo/local-src/cosanum_delivery/models/res_partner_delivery_info.py
+++ odoo/local-src/cosanum_delivery/models/res_partner_delivery_info.py
@@ -2,6 +2,8 @@
 # License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl.html).
 from odoo import fields, models
 
+# TODO: move to new module `delivery_carrier_customer_info]
+
 
 class ResPartnerDeliveryInfo(models.Model):
     _name = "res.partner.delivery.info"
