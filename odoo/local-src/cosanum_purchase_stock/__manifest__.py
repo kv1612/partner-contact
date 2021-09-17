5a695d1d319c79553e6e380ff2b0dd151e32a4e5
Sébastien Alix <sebastien.alix@camptocamp.com>
2021-09-17 15:00:41 +0200

    Migrate stock warehouse data (purchase_stock)
    Add stock warehouse data related to `purchase_stock` in the module
    `cosanum_purchase_stock`.
    
    BSCOS-2797

--- odoo/local-src/cosanum_purchase_stock/__manifest__.py
+++ odoo/local-src/cosanum_purchase_stock/__manifest__.py
@@ -1,21 +1,24 @@
 # Copyright 2019-2021 Camptocamp SA
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html).
 {
-    "name": "Cosanum Stock Warehouse",
-    "summary": "Base module hosting Cosanum stock warehouse",
+    "name": "Cosanum Purchase Stock",
+    "summary": "Cosanum Purchase Stock integration",
     "version": "14.0.1.0.0",
     "category": "Hidden",
     "author": "Camptocamp",
     "license": "AGPL-3",
     "depends": [
         # core
-        "stock",
-        "mrp",
-        "mrp_subcontracting",
+        "purchase_stock",
+        # local-src
+        "cosanum_stock_warehouse",
     ],
     "website": "https://www.camptocamp.com",
     "data": [
         "data/stock_warehouse.xml",
     ],
+    # NOTE: as original records are flagged with 'noupdate', the only way to get
+    # them updated is to read the XML data file manually when the module is installed
+    "post_init_hook": "update_data",
     "installable": True,
 }
